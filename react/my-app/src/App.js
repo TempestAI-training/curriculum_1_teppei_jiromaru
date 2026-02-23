c0aa112a9842c03c50a8aae7278ad458f6cc70cc
jiromaru teppei <teppei19260@gmail.com>
2026-02-23 13:24:01 +0900

    親子コンポーネントの連携まで

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import MemoForm from "./MemoForm";
 
 function App() {
   const [isVisible, setIsVisible] = useState(true);
@@ -27,42 +28,15 @@ function App() {
           このアプリについて２
         </button>
         {isVisible && (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 10,
-              padding: 20,
-              border: "1px solid #61dafb",
-              marginTop: 20,
-              width: 300,
-            }}
-          >
-            <label>
-              title
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </label>
-            <label>
-              content
-              <textarea
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-              />
-            </label>
-            <label>
-              tag
-              <input
-                type="text"
-                value={tag}
-                onChange={(e) => setTag(e.target.value)}
-              />
-            </label>
-            <button onClick={handleSave}>save</button>
-          </div>
+          <MemoForm
+            title={title}
+            setTitle={setTitle}
+            content={content}
+            setContent={setContent}
+            tag={tag}
+            setTag={setTag}
+            onSave={handleSave}
+          />
         )}
       </header>
     </div>
